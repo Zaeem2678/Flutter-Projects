f9844c6a9441f7385be4ffcc12b1b3087185af86
Zaeem2678 <102031010+Zaeem2678@users.noreply.github.com>
2024-11-03 03:56:50 -0800

    Update main.dart

--- expense_tracker/lib/main.dart
+++ expense_tracker/lib/main.dart
@@ -1,9 +1,21 @@
 import 'package:expense_tracker/expenses.dart';
 import 'package:flutter/material.dart';
 
+var kColorScheme =
+    ColorScheme.fromSeed(seedColor: const Color.fromARGB(255, 16, 6, 44));
+
 void main() {
-  runApp(const MaterialApp(
+  runApp(MaterialApp(
+    theme: ThemeData(
+      colorScheme: kColorScheme,
+      appBarTheme: const AppBarTheme(
+          backgroundColor: Color.fromARGB(255, 10, 11, 71),
+          foregroundColor: Colors.white),
+      cardTheme: const CardTheme(
+          color: Color.fromARGB(255, 207, 195, 209),
+          margin: EdgeInsets.symmetric(horizontal: 14, vertical: 8)),
+    ),
     debugShowCheckedModeBanner: false,
-    home: Expenses(),
+    home: const Expenses(),
   ));
 }
